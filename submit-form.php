ad20795a5450d5efad266329057f2012274b75c7
kusumrawat1981 <126628586+kusumrawat1981@users.noreply.github.com>
2023-05-18 19:21:11 +0100

    Update submit-form.php

--- submit-form.php
+++ submit-form.php
@@ -1,34 +1,31 @@
 <?php
-// Check if the form has been submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Get the form data
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-  // Create the email body
-  $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    // Set recipient email address
+    $to = "your-email@example.com";
 
-  // Set the recipient email address(es)
-  $to = 'learndigitallywithme@gmail.com';
+    // Set email subject
+    $subject = "New Enquiry";
 
-  // Set the email subject
-  $subject = "Contact Form Submission - $subject";
+    // Compose email content
+    $email_content = "Name: $name \n";
+    $email_content .= "Email: $email \n";
+    $email_content .= "Message: $message \n";
 
-  // Set the email headers
-  $headers = "From: $name <$email>\r\nReply-To: $email\r\n";
+    // Set additional headers
+    $headers = "From: $email \r\n";
+    $headers .= "Reply-To: $email \r\n";
 
-  // Send the email
-  if (mail($to, $subject, $body, $headers)) {
-    // Email sent successfully
-    echo "Thank you for your message!";
-  } else {
-    // Error sending email
-    echo "Sorry, there was an error sending your message. Please try again later.";
-  }
-} else {
-  // Form not submitted
-  echo "Sorry, the form was not submitted.";
+    // Send email
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "Thank you for your enquiry!";
+    } else {
+        echo "Oops! Something went wrong.";
+    }
 }
 ?>
+
