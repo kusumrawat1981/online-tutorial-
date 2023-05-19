11e0f9a124c97352081c166ae5e03fe520943c87
kusumrawat1981 <126628586+kusumrawat1981@users.noreply.github.com>
2023-05-19 10:22:44 +0100

    Update submit-form.php

--- submit-form.php
+++ submit-form.php
@@ -1,48 +1,21 @@
 <?php
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-require 'PHPMailer/src/Exception.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-$mail = new PHPMailer(true);
-
-// Enable verbose debug output (optional)
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-// Set the mailer to use SMTP
-$mail->isSMTP();
-
-// Specify the SMTP server
-$mail->Host = 'smtp.gmail.com';
-
-// Enable SMTP authentication
-$mail->SMTPAuth = true;
-
-// Set the username and password for your Gmail account
-$mail->Username = 'learndigitallywithme@gmail.com';
-$mail->Password = 'pkbd@2408';
-
-// Enable TLS encryption
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-
-// Set the SMTP port (587 for TLS, 465 for SSL)
-$mail->Port = 587;
-
-// Set the sender and recipient email addresses
-$mail->setFrom('kusumrawat1981@gmail.com', 'Your Name');
-$mail->addAddress('kusumrawat1981@gmail.com', 'Recipient Name');
-// Set email subject and body
-$mail->Subject = 'Enquiry';
-$mail->Body = 'This is the content of the enquiry email.';
-
-// Send the email
-if ($mail->send()) {
-    echo 'Email sent successfully!';
-} else {
-    echo 'Error sending email: ' . $mail->ErrorInfo;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Retrieve form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  // Set up email
+  $to = "your-email@example.com"; // Replace with your email address
+  $subject = "New Enquiry";
+  $body = "Name: $name\nEmail: $email\nMessage: $message";
+  $headers = "From: $email";
+  
+  // Send email
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Thank you for your enquiry!";
+  } else {
+    echo "Oops! Something went wrong.";
+  }
 }
-
 ?>
-
