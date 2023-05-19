3d1fb03105495c29d2b22cae57db7cb1a13a1693
kusumrawat1981 <126628586+kusumrawat1981@users.noreply.github.com>
2023-05-19 12:14:49 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,24 +1,47 @@
 <?php
-require 'PHPMailerAutoload.php';
-$mail = new PHPMailer;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+require 'path/to/PHPMailer/src/Exception.php';
 
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->SMTPAuth = true;
-$mail->Username = 'learndigitallywithme@gmail.com';
-$mail->Password = 'pkbd@2408';
-$mail->SMTPSecure = 'tls';
-$mail->setFrom('your-email@gmail.com', 'Your Name');
-$mail->addAddress('learndigitallywithme@gmail.com', 'Recipient Name');
-$mail->Subject = 'New Enquiry';
-$mail->Body = 'This is a test email from the enquiry form.';
+require 'vendor/autoload.php'; // Path to PHPMailer autoloader
 
-if (!$mail->send()) {
-    echo 'Oops! Something went wrong.';
-    echo 'Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Thank you for your enquiry!';
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Instantiate PHPMailer
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Replace with your SMTP server
+        $mail->Port = 587; // Replace with your SMTP port
+        $mail->SMTPAuth = true;
+        $mail->Username = 'kusumrawat1981e@gmail.com'; // Replace with your email address
+        $mail->Password = 'pkbd@2408'; // Replace with your email password
+        $mail->SMTPSecure = 'tls';
+
+        // Sender and recipient
+        $mail->setFrom($email, $name);
+        $mail->addAddress('learndigitallywithme@gmail.com'); // Replace with recipient's email address
+
+        // Email content
+        $mail->isHTML(false);
+        $mail->Subject = 'New Enquiry';
+        $mail->Body = "Name: $name\nEmail: $email\nMessage: $message";
+
+        // Send email
+        $mail->send();
+        echo 'Thank you for your enquiry!';
+    } catch (Exception $e) {
+        echo 'Oops! Something went wrong.';
+        echo 'Error: ' . $mail->ErrorInfo;
+    }
 }
-
 ?>
