37542560fc1cb3b2d7cd8d9e4ca161831e67db18
kusumrawat1981 <126628586+kusumrawat1981@users.noreply.github.com>
2023-05-20 16:07:31 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,9 +1,9 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require '/PHPMailer/src/PHPMailer.php';
-require '/PHPMailer/src/SMTP.php';
-require '/PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
 
 require 'vendor/autoload.php'; // Path to PHPMailer autoloader
 
